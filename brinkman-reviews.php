e2b6b3deadd87f26cb366113cd3b1e2770d7fc14
Jesse James Burton <jesse@burtonmediainc.com>
2019-11-05 17:38:02 -0600

    Could not get the updater to work correctly, trying another approach

--- brinkman-reviews.php
+++ brinkman-reviews.php
@@ -180,23 +180,4 @@ function lowDash($value){
   return strtolower(preg_replace('/[[:space:]]+/', '-', $value));
 }
 
-/* UPDATES */
-include_once('updater.php');
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-  $config = array(
-    'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-    'proper_folder_name' => 'brinkman-reviews', // this is the name of the folder your plugin lives in
-    'api_url' => 'https://api.github.com/repos/jessejburton/Brinkman-Reviews', // the GitHub API url of your GitHub repo
-    'raw_url' => 'https://raw.github.com/jessejburton/Brinkman-Reviews/master', // the GitHub raw url of your GitHub repo
-    'github_url' => 'https://github.com/jessejburton/Brinkman-Reviews', // the GitHub url of your GitHub repo
-    'zip_url' => 'https://github.com/jessejburton/Brinkman-Reviews/archive/master.zip', // the zip url of the GitHub repo
-    'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-    'requires' => '3.0', // which version of WordPress does your plugin require?
-    'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-    'readme' => 'README.md', // which file to use as the readme for the version number
-    'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
-  );
-  new WP_GitHub_Updater($config);
-}
-
 ?>
